eb33aba8e8902dfac1c6f46851a7e4250c26d363
Shubham Singh <shubhammahar1306@gmail.com>
2024-12-21 10:12:52 +0530

    working on how logic is added to ping
    Signed-off-by: Shubham Singh <shubhammahar1306@gmail.com>

--- cmd/net/ping.go
+++ cmd/net/ping.go
@@ -10,23 +10,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	urlPath string
+)
+
 // pingCmd represents the ping command
 var pingCmd = &cobra.Command{
 	Use:   "ping",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "this pings a remote URL and returns the response",
+	Long: ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("ping called")
+		// Logic comes here
 	},
 }
 
 func init() {
+	pingCmd.Flags().StringVarP(&urlPath, "url", "u", "", "The URL to ping")
 	NetCmd.AddCommand(pingCmd)
+
+	// to set a flag as required
+	err := NetCmd.MarkFlagRequired("url")
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	// Here you will define your flags and configuration settings.
 
